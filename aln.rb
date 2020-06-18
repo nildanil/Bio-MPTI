cccc88ec571594fd81871532a82b9f96b20f6263
nildanil <64488861+nildanil@users.noreply.github.com>
2020-06-18 12:26:08 +0300

    Add files via upload

---
+++ aln.rb
@@ -0,0 +1,22 @@
+require 'bio' #подключаем библиотеку bio, чтобы биоруби заработал
+count_of_dismatches = 0 #счетчик мутаций
+num_of_nucleotides_in_aln_file = 683269 #константа, количество нуклеотидов в файле(номер последнего)
+list_of_numbers_of_dismatches = Array.new #создаем список всех индексов мутаций(пока что пустой)
+aln = Bio::ClustalW::Report.new(File.read('alignments/compile1.aln')) #читаем файл aln с помощью инструмента ClustalW
+match_array = aln.match_line[1..num_of_nucleotides_in_aln_file].split("") #записываем строку
+
+match_array.each_with_index do |symbol,index |   #цикл, который все считает
+    if symbol == " "
+      count_of_dismatches += 1
+      list_of_numbers_of_dismatches << index
+      end
+end #тут цикл кончился
+
+if count_of_dismatches == 0 #если счетчик мутаций не сдвинулся с места, то выводим сообщение
+  puts "the alignments are gomologic"
+  puts "no dismatches"
+else #иначе выводим номера мутаций и их количество
+  puts "the places, where mutation was found:"
+  puts list_of_numbers_of_dismatches.join("|")
+  puts count_of_dismatches
+end
